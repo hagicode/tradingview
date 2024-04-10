97077870fc43a2456b2aea59a04bb516803963ef
hagicode <131940602+hagicode@users.noreply.github.com>
2024-04-10 21:48:04 +0900

    Update tradingview_app.py

--- tradingview_app.py
+++ tradingview_app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import requests
+import streamlit.components.v1 as stc
 
-# ウェブサイトのURL
-url = "https://sekai-kabuka.com/shikyou.html"
-
-# ウェブサイトの内容を取得
-response = requests.get(url)
-
-# Streamlitで表示
-st.write(response.text)
+stc.iframe("https://sekai-kabuka.com/shikyou.html",scrolling=True)
