331caf49c2d19ee3663d5f79b0bfcb64c78cfe2e
hagicode <131940602+hagicode@users.noreply.github.com>
2024-04-10 21:32:22 +0900

    Update tradingview_app.py

--- tradingview_app.py
+++ tradingview_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import requests
 
-html_code = """
-<div class="tradingview-widget-container"><div class="tradingview-widget-container__widget"></div><div class="tradingview-widget-copyright"><a href="https://jp.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">TradingViewですべてのマーケットを追跡</span></a></div><script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>{"symbols": [{"description": "ドル円","proName": "OANDA:USDJPY"},{"description": "WTI原油先物","proName": "MATBAROFEX:WTI1!"},{"description": "S&P500","proName": "FRED:SP500"},{"description": "VIX","proName": "CAPITALCOM:VIX"}],"isTransparent": false,"showSymbolLogo": true,"colorTheme": "light","locale": "ja"}</script></div>
-"""
+# ウェブサイトのURL
+url = "https://sekai-kabuka.com/shikyou.html"
 
-st.markdown(html_code, unsafe_allow_html=True)
+# ウェブサイトの内容を取得
+response = requests.get(url)
+
+# Streamlitで表示
+st.write(response.text)
